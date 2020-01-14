17852adf4b4e56cda7a90a68ea1485f88a21c6cb
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-14 12:26:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,16 +1,19 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1, 2, 3 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
+  array
 end
 
 def add_element_to_start_of_array(array, element)
+  array.unshift(element)
+  array
   
 end
 
